ffa8d32c1d5f36efc8988db444f01830e3caacd3
DESRUET Antoine <antoine.desruet@epitech.eu>
2020-02-17 17:26:53 +0100

    [ADD] adding lib

---
+++ include/navy.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** navy_test
+** File description:
+** navy
+*/
+
+#ifndef NAVY_H_
+#define NAVY_H_
+
+#include "my.h"
+
+char **create_board(void);
+void display_board(char **board);
+
+#endif /* !NAVY_H_ */
