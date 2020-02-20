b07be51d58799a7b101304fbc8d63a70835a2c93
DESRUET Antoine <antoine.desruet@epitech.eu>
2020-02-20 22:18:45 +0100

    [ADD] adding function to catch signal and interpret it

--- get_signal.c
+++ get_signal.c
@@ -5,31 +5,54 @@
 ** signal
 */
 
-#include "navy.h"
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-static const char letter[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+int recep_sig = 0;
 
-void recep_signal(int sig)
+void handler(int sig, siginfo_t *info, void *ucontext)
 {
-    const struct sigaction *act;
-    struct sigaction *oldact;
-    sigaction(sig, act, oldact);
-    printf("signal = %d\n", sig);
+    if (sig == SIGUSR1)
+        recep_sig = 1;
+    if (sig == SIGUSR2)
+        recep_sig = 0;
+    kill(info->si_pid, SIGUSR1);
+}
+
+char *clean_bin(char *bin)
+{
+    int len = my_strlen(bin);
+
+    for (; len >= 0; len--) {
+        if (bin[len] == '0' && bin[len - 1] == '1') {
+            bin[len] = 0;
+            return (bin);
+        }
+    }
+    return (bin);
 }
 
 void get_mypid(void)
 {
-    printf("pid = %d\n", getpid());
-    while (1)
-        signal(SIGUSR1, recep_signal);
-}
-
-int main(int ac, char **av)
-{
-        get_mypid();
-        return (0);
+    struct sigaction act;
+    int pid = getpid();
+    int i = 0;
+    char *bin = malloc(sizeof(char)*5);
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = &handler;
+    sigaction(SIGUSR1, &act, NULL);
+    sigaction(SIGUSR2, &act, NULL);
+    my_printf("wait pid = %d\n", pid);
+    while (i < 4) {
+        pause();
+        bin[i] = recep_sig + '0';
+        printf("i == %d\n", i);
+        i++;
+    }
+    bin[4] = 0;
+    bin = clean_bin(bin);
+    my_printf("binary = %d\n", my_btoi(my_revstr(bin)));
 }
