f9e9b83c6c1b9cb622b014e21a12e8bba567a0d0
DESRUET Antoine <antoine.desruet@epitech.eu>
2020-02-21 04:33:10 +0100

    [ADD] ping ping like funtion to send position

--- src/signal.c
+++ src/signal.c
@@ -13,13 +13,19 @@
 
 int recep_sig = 0;
 
-void handler(int sig, siginfo_t *info, void *ucontext)
+static void handler(int sig, siginfo_t *info, void *ucontext)
 {
     if (sig == SIGUSR1)
         recep_sig = 1;
     if (sig == SIGUSR2)
         recep_sig = 0;
     kill(info->si_pid, SIGUSR1);
+}
+
+static void handler_return(int sig, siginfo_t *info, void *ucontext)
+{
+    if (sig == SIGUSR1)
+        recep_sig += 1;
 }
 
 char *clean_bin(char *bin)
@@ -35,17 +41,15 @@ char *clean_bin(char *bin)
     return (bin);
 }
 
-void get_mypid(void)
+int get_signal(void)
 {
     struct sigaction act;
-    int pid = getpid();
     int i = 0;
     char *bin = malloc(sizeof(char)*5);
     act.sa_flags = SA_SIGINFO;
     act.sa_sigaction = &handler;
     sigaction(SIGUSR1, &act, NULL);
     sigaction(SIGUSR2, &act, NULL);
-    my_printf("wait pid = %d\n", pid);
     while (i < 4) {
         pause();
         bin[i] = recep_sig + '0';
@@ -53,5 +57,21 @@ void get_mypid(void)
     }
     bin[4] = 0;
     bin = clean_bin(bin);
-    my_printf("binary = %d\n", my_btoi(my_revstr(bin)));
+    return (my_btoi(my_revstr(bin)));
+}
+
+void my_send_sig(int data, int pid)
+{
+    struct sigaction act;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = &handler_return;
+    recep_sig = 0;
+    sigaction(SIGUSR1, &act, NULL);
+    for (; recep_sig <= 3;) {
+        if ((data >> recep_sig) % 2 == 1)
+            kill(pid, SIGUSR1);
+        if ((data >> recep_sig) % 2 == 0)
+            kill(pid, SIGUSR2);
+        usleep(500);
+    }
 }
